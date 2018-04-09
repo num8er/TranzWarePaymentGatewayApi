c5085315b92529813c026e80a07fb0acd1923b02
Anar Jafarov <anar.k.jafarov@gmail.com>
2018-04-09 16:36:52 +0300

    added composer.json to samples for copy pasting of usage sample

--- samples/order_approved.php
+++ samples/order_approved.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 use num8er\TranzWarePaymentGateway\Handlers\TranzWarePaymentGatewayOrderCallbackHandler;
 
